a8d9a3432fe259ddb3cb2e499f2c10b36fb909e0
Tyler Sinks <tyler.sinks@gmail.com>
2021-01-29 14:53:15 -0500

    pass all specs sans CalcTotal

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,25 @@
 function Pizza() {
   this.size = "large";
   this.toppings = [];
+  this.smallTopEach = 2;
+  this.largeTopEach = 4;
   this.largeBasePrice = 38;
   this.smallBasePrice = 26;
+  this.name = "orphan";
 }
-function Order(pizza) {
-  this.diablo = pizza;
+Pizza.prototype.assignName = function(name = "not specified") {
+  const defaultNames = ["plain", "baz", "bat", "bar", "diablo", "foo", "wasabi", "Georgian", "Jersey", "espresso", "caprice"];
+  if (name === "not specified") {
+    let randomNumInRange = Math.floor(Math.random() * 10) + 1;
+    this.name = defaultNames[randomNumInRange];
+  } else {
+    this.name = name;
+  }
 }
-Order.prototype.addPizza = function(pizzaInstance) {
-  this.pizza1 = pizzaInstance;
+function Order() {
+  this.allPizzas = [];
+  this.salesTax = 1.08875;
+}
+Order.prototype.addPizza = function(newPizza) {
+  this.allPizzas.push(newPizza);
 }
